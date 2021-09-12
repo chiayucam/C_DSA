9b0f1c3e000f67719478ac204e10bf9e55a40b26
chiayucam <48814609+chiayucam@users.noreply.github.com>
2021-09-13 06:37:38 +0800

    Create 5_15.c

---
+++ chap05/5_15.c
@@ -0,0 +1,72 @@
+// recursive search in binary search tree
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct BSTreeNode
+{
+    struct BSTreeNode *leftchild;
+    int data;
+    struct BSTreeNode *rightchild;
+};
+
+struct BSTreeNode *root;
+
+struct BSTreeNode *searchBST(struct BSTreeNode *node, int x)
+{
+    if (node == NULL)
+    {
+        return NULL;
+    }
+    if (x == node->data)
+    {
+        return node;
+    }
+    if (x < node->data)
+    {
+        return searchBST(node->leftchild, x);
+    }
+    return searchBST(node->rightchild, x);
+}
+
+void createLeftchild(struct BSTreeNode *node, int element)
+{
+    struct BSTreeNode *left;
+    left = (struct BSTreeNode *)malloc(sizeof(struct BSTreeNode));
+    node->leftchild = left;
+    left->data = element;
+    left->leftchild = NULL;
+    left->rightchild = NULL;
+}
+
+void createRightchild(struct BSTreeNode *node, int element)
+{
+    struct BSTreeNode *right;
+    right = (struct BSTreeNode *)malloc(sizeof(struct BSTreeNode));
+    node->rightchild = right;
+    right->data = element;
+    right->leftchild = NULL;
+    right->rightchild = NULL;
+}
+
+void main()
+{
+    root = (struct BSTreeNode *)malloc(sizeof(struct BSTreeNode));
+    root->data = 14;
+    createLeftchild(root, 7);
+    createLeftchild(root->leftchild, 2);
+    createRightchild(root->leftchild, 10);
+    createRightchild(root, 16);
+    createRightchild(root->rightchild, 20);
+
+    struct BSTreeNode *searchedNode;
+    searchedNode = searchBST(root, 20);
+    if (searchedNode == NULL)
+    {
+        printf("No matching item found");
+    }
+    else
+    {
+        printf("Found matching search at %p with value %d", searchedNode, searchedNode->data);
+    }
+}
